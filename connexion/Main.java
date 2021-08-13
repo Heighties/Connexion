be5d55cefa18d699edd308552d011e5314bd434a
Heighties <heighties@gmail.com>
2021-08-14 01:00:56 +0200

    scanner + System.exit (voir "try")

--- connexion/Main.java
+++ connexion/Main.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-	private static Scanner scanner = new Scanner(System.in);
+	// private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Entrez votre identifiant : ");
-		String identifiant = scanner.nextLine();
-		System.out.println("Entrez votre mot de passe : ");
-		String mdp = scanner.nextLine();
-		if (identifiant.equals("Geoffrey") && mdp.equals("motdepassedeGeoffrey64")) {
-			System.out.println("Connecté");
 
-		} else {
-			System.out.println("Echec de connexion");
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Entrez votre identifiant : ");
 
+			String identifiant = scanner.nextLine();
+			System.out.println("Entrez votre mot de passe : ");
+			String mdp = scanner.nextLine();
+			if (identifiant.equals("Geoffrey") && mdp.equals("motdepassedeGeoffrey64")) {
+				System.out.println("Connecté");
+			} else {
+				System.out.println("Echec de connexion");
+				System.exit(-1);
+			}
+			System.out.println("Bienvenue Geoffrey");
+			System.out.println("Vos infos persos sont : .........");
 		}
-
 	}
-
 }
